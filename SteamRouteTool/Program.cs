33cd48bf209ecd485c39f19fd77a078f8046745e
Nenad <nenad@1911.su>
2025-12-18 01:01:30 +0100

    Update Program.cs

--- SteamRouteTool/Program.cs
+++ SteamRouteTool/Program.cs
@@ -1,22 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace SteamRouteTool
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
         [STAThread]
         static void Main()
         {
+            // Allocate a console to see errors/output
+            AllocConsole();
+            Console.WriteLine("SteamRouteTool starting...");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+
+            try
+            {
+                Application.Run(new Main());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex);
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
         }
     }
 }
