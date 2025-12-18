e202a0431a4e700edc0ad6cd6276d0126fff8f05
Nenad <nenad@1911.su>
2025-12-18 02:11:41 +0100

    Update Program.cs

--- SteamRouteTool/Program.cs
+++ SteamRouteTool/Program.cs
@@ -1,38 +1,21 @@
-using System;
-using System.IO;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
-
-namespace SteamRouteTool
+[STAThread]
+static void Main()
 {
-    static class Program
+    // AllocConsole(); // REMOVE THIS
+    // Console.WriteLine("SteamRouteTool starting..."); // REMOVE THIS
+
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+
+    try
     {
-        [DllImport("kernel32.dll")]
-        private static extern bool AllocConsole();
-
-        [STAThread]
-        static void Main()
-        {
-            AllocConsole();
-            Console.WriteLine("SteamRouteTool starting...");
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            try
-            {
-                Application.Run(new Main());
-            }
-            catch (Exception ex)
-            {
-                File.WriteAllText("SteamRouteTool_Error.txt", ex.ToString());
-                Console.WriteLine("Exception written to SteamRouteTool_Error.txt");
-            }
-            finally
-            {
-                Console.WriteLine("Press Enter to exit...");
-                Console.ReadLine();
-            }
-        }
+        Application.Run(new Main());
     }
+    catch (Exception ex)
+    {
+        File.WriteAllText("SteamRouteTool_Error.txt", ex.ToString());
+        // Console.WriteLine("Exception written to SteamRouteTool_Error.txt"); // REMOVE THIS
+    }
+
+    // Console.ReadLine(); // REMOVE THIS
 }
